d3830a4e1e35eb8915c610d6aeda7f11026f7ac4
lydiabarham <lcbarham9@gmail.com>
2023-12-20 20:34:53 +0000

    add files and folder structure

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+const API_KEY = 'bc9962c147msh449db8706885ddfp12c772jsn032f560cfdbc';
+      const movieId = '109445';
+      const url = `https://watchthis.p.rapidapi.com/api/v1/movie?ids=${movieId}`;
+      const options = {
+          method: 'GET',
+          headers: {
+              'X-RapidAPI-Key': API_KEY,
+              'X-RapidAPI-Host': 'watchthis.p.rapidapi.com',
+          },
+      };
+      fetch(url, options)
+          .then((response) => response.json())
+          .then((data) => {
+              console.log('Movie Data:', data);
+              // Process the movie data here
+          });
