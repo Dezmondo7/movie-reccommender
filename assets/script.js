45c4b8e4c6fcd86c61ce6b3eb1fe82ba22cb1252
SharpLad <143443228+yrezvani@users.noreply.github.com>
2023-12-22 18:53:20 +0000

    Add initial api call for searching for movies

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,27 @@
-const API_KEY = 'bc9962c147msh449db8706885ddfp12c772jsn032f560cfdbc';
-const movieId = '109445';
-const url = `https://watchthis.p.rapidapi.com/api/v1/movie?ids=${movieId}`;
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': 'watchthis.p.rapidapi.com',
-    },
-};
-fetch(url, options)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Movie Data:', data);
-        // Process the movie data here
+$(document).ready(function () {
+    $('#searchButton').on('click', function (e) {
+        e.preventDefault();
+        const movieName = $('#movieNameInput').val();
+        const movieYear = $('#movieYearInput').val();
+
+        searchMovie(movieName, movieYear);
     });
+});
+
+function searchMovie(movieName, movieYear) {
+    const apiKey = '06913f82fcdc1886d498b562028e1b66';
+
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieName}`;
+
+    if (movieYear) {
+        url += `&year=${movieYear}`;
+    }
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            displaySearchResults(data.results);
+        });
+}
