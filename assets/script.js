5f712cdf8e6dc0e5ca576e37bf001c294ac1e7b0
Jennifer Parsons <admin@Jennifer.local>
2023-12-23 23:18:34 +0000

    styling made for carousel

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
-const API_KEY = 'bc9962c147msh449db8706885ddfp12c772jsn032f560cfdbc';
+// trigger Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+//const API_KEY = 'bc9962c147msh449db8706885ddfp12c772jsn032f560cfdbc';
       const movieId = '109445';
       const url = `https://watchthis.p.rapidapi.com/api/v1/movie?ids=${movieId}`;
       const options = {
